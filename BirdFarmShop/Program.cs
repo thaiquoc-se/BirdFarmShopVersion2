6530b3cfeb42893e5753b8ecd6c2658c44ff2433
Thaiquocse <Thaiquocse@gmail.com>
2023-10-15 19:46:57 +0700

    Add project files.

---
+++ BirdFarmShop/Program.cs
@@ -0,0 +1,30 @@
+using BusinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<BirdFarmShopContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BirdFarmShop") ?? throw new InvalidOperationException("Connection string 'BirdFarmShop' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ DataAccessObjects/UserDAO.cs
@@ -0,0 +1,69 @@
+﻿using BusinessObjects.DTOs;
+using BusinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessObjects
+{
+    public class UserDAO
+    {
+        private static UserDAO instance = null!;
+
+        private static readonly object instanceLock = new object();
+        private readonly BirdFarmShopContext _context;
+
+        public UserDAO()
+        {
+            _context = new BirdFarmShopContext();
+        }
+
+        public static UserDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new UserDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public List<UserDTO> GetAllUsers()
+        {
+            var users = _context.TblUsers
+                .Include(t => t.District)
+                .Include(t => t.Role)
+                .Include(t => t.Ward).ToList();
+
+            List<UserDTO> userList = new List<UserDTO>();
+            foreach (var user in users)
+            {
+                var _user = new UserDTO()
+                {
+                    UserId = user.UserId,
+                    UserName = user.UserName,
+                    UserAddress = user.UserAddress + ", " + user.Ward!.WardName + ", " + user.District!.DistrictName,
+                    FullName = user.FullName,
+                    UserStatus = user.UserStatus,
+                    RoleId = user.RoleId,
+                    RoleName = user.Role.RoleName!,
+                    Pass = user.Pass,
+                    Email = user.Email,
+                    Phone = user.Phone,
+                };
+
+                userList.Add(_user);
+            }
+
+            return userList;
+        }
+    }
+}
