d1adb12b66da8c3708890574dfafef1bb81d5685
Thaiquocse <Thaiquocse@gmail.com>
2023-10-30 13:16:52 +0700

    Done Order Management (Admin,Manager)

---
+++ BirdFarmShop/Pages/Manager/OrderManagement/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects.Models;
+using Services.IServices;
+
+namespace BirdFarmShop.Pages.Manager.OrderManagement
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IOrderService _orderService;
+        private string isManager;
+
+        public IndexModel(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IList<TblOrder> TblOrder { get;set; } = default!;
+
+        public IActionResult OnGet()
+        {
+            try
+            {
+                isManager = HttpContext.Session.GetString("isManager")!;
+                if (isManager != "MN")
+                {
+                    return NotFound();
+                }
+                if (isManager == null)
+                {
+                    return NotFound();
+                }
+            }
+            catch
+            {
+                NotFound();
+            }
+            TblOrder = _orderService.GetAllOrders();
+            return Page();
+        }
+    }
+}
